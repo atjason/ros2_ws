9e55ecf3018b1369a013a450a551365785ea4b80
Jason <quietjosen@gmail.com>
2025-03-27 08:34:59 +0800

    Add client.

---
+++ src/py_srvcli/py_srvcli/client_member_function.py
@@ -0,0 +1,36 @@
+from example_interfaces.srv import AddTwoInts
+
+import rclpy
+from rclpy.node import Node
+
+import sys
+
+class MinimalClientAsync(Node):
+  def __init__(self):
+    super().__init__('minimal_client_async')
+    self.client = self.create_client(AddTwoInts, 'add_two_ints')
+    while not self.client.wait_for_service(timeout_sec=1.0):
+      self.get_logger().info('Service not available. Waiting…')
+    
+    self.request = AddTwoInts.Request()
+    
+  def send_request(self, a, b):
+    self.request.a = a
+    self.request.b = b
+    self.client.call_async(self.request)
+    
+def main():
+  rclpy.init()
+  
+  client = MinimalClientAsync()
+  future = client.send_request(int(sys.argv[1]), int(sys.argv[2]))
+  rclpy.spin_until_future_complete(client, future)
+  response = future.result()
+  client.get_logger().info(
+    'Result of add_two_ints: %d + %d = %d' %
+    (int(sys.argv[1]), int(sys.argv[2]), response.sum)
+  )
+  
+  client.destroy_node()
+  rclpy.shutdown()
+    
--- src/py_srvcli/py_srvcli/service_member_function.py
+++ src/py_srvcli/py_srvcli/service_member_function.py
@@ -3,21 +3,24 @@ from example_interfaces.srv import AddTwoInts
 import rclpy
 from rclpy.node import Node
 
-class MininumService(Node):
+class MinimalService(Node):
   def __init__(self):
-    super().__init__('mininum_serivce')
+    super().__init__('minimal_serivce')
     self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
 
   def add_two_ints_callback(self, request, response):
     response.sum = request.a + request.b
-    sef.get_logger().info('Incomming request\na: %d b: %d' % (request.a, request.b))
+    self.get_logger().info('Incomming request\na: %d b: %d' % (request.a, request.b))
 
     return response
   
 def main():
   rclpy.init()
-  node = MinimumSerivce()
+  
+  node = MinimalService()
+  node.get_logger().info('Service starts…')
   rclpy.spin(node)
+  
   rclpy.shutdown()
 
 if __name__ == '__main__':
