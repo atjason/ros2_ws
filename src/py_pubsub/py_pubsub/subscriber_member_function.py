45ece6a1fa75bd6e405439815d4ecdde42a2e745
Jason <quietjosen@gmail.com>
2025-03-27 09:07:49 +0800

    Use custom msg for publish subscriber.

--- src/py_pubsub/py_pubsub/subscriber_member_function.py
+++ src/py_pubsub/py_pubsub/subscriber_member_function.py
@@ -17,20 +17,22 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
+from tutorial_interfaces import Num
+
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
         self.subscription = self.create_subscription(
-            String,
+            Num,
             'topic',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.get_logger().info('I heard: "%d"' % msg.num)
 
 
 def main(args=None):
