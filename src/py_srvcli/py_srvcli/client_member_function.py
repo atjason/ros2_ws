4ca3c100a0fc3b32fbd7aa09fd3f8fb26e70b491
Jason <quietjosen@gmail.com>
2025-03-27 09:16:17 +0800

    Use AddThreeInts.

--- src/py_srvcli/py_srvcli/client_member_function.py
+++ src/py_srvcli/py_srvcli/client_member_function.py
@@ -1,4 +1,5 @@
 from example_interfaces.srv import AddTwoInts
+from tutorial_interfaces.srv import AddThreeInts
 
 import rclpy
 from rclpy.node import Node
@@ -9,27 +10,28 @@ class MinimalClientAsync(Node):
   
   def __init__(self):
     super().__init__('minimal_client_async')
-    self.client = self.create_client(AddTwoInts, 'add_two_ints')
+    self.client = self.create_client(AddThreeInts, 'add_three_ints')
     while not self.client.wait_for_service(timeout_sec=1.0):
       self.get_logger().info('Service not available. Waiting…')
     
-    self.request = AddTwoInts.Request()
+    self.request = AddThreeInts.Request()
     
-  def send_request(self, a, b):
+  def send_request(self, a, b, c):
     self.request.a = a
     self.request.b = b
+    self.request.c = c
     return self.client.call_async(self.request)
     
 def main():
   rclpy.init()
   
   client = MinimalClientAsync()
-  future = client.send_request(int(sys.argv[1]), int(sys.argv[2]))
+  future = client.send_request(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
   rclpy.spin_until_future_complete(client, future)
   response = future.result()
   client.get_logger().info(
-    'Result of add_two_ints: %d + %d = %d' %
-    (int(sys.argv[1]), int(sys.argv[2]), response.sum)
+    'Result of add_two_ints: %d + %d + %d = %d' %
+    (int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), response.sum)
   )
   
   client.destroy_node()
