023d29d41daed2abbfcc9eda0792244de5221282
Jason <quietjosen@gmail.com>
2025-03-28 09:30:49 +0800

    Send feedback.

--- src/action_demo/fibonacci_action_server.py
+++ src/action_demo/fibonacci_action_server.py
@@ -1,3 +1,4 @@
+import time
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
@@ -18,14 +19,19 @@ class FibonacciActionServer(Node):
   def execute_callback(self, goal_handle):
     self.get_logger().info('Executing goal…')
     
-    sequence = [0, 1]
+    feedback_msg = Fibonacci.Feedback()
+    feedback_msg.partial_sequence = [0, 1]
     
     for i in range(1, goal_handle.request.order):
-      sequence.append(sequence[i] + sequence[i-1])
+      feedback_msg.partial_sequence.append(
+        feedback_msg.partial_sequence[i] + feedback_msg.partial_sequence[i-1])
+      self.get_logger().info('Feedback: {0}'.format(feedback_msg.partial_sequence))
+      goal_handle.publish_feedback(feedback_msg)
+      time.sleep(1)
       
     goal_handle.succeed()
     result = Fibonacci.Result()
-    result.sequence = sequence
+    result.sequence = feedback_msg.partial_sequence
     return result
 
 def main(args=None):
