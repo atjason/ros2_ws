b2ed3d16ed4ac0c782fdfceb413626420b5658a1
Jason <quietjosen@gmail.com>
2025-04-02 10:46:50 +0800

    Update launch.

--- src/launch_tutorial/launch/launch_turtlesim.launch.py
+++ src/launch_tutorial/launch/launch_turtlesim.launch.py
@@ -8,12 +8,12 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import PushRosNamespace
 
 def generate_launch_description():
-  launch_dir = os.path.join(get_package_share_directory('launch_tutorial', 'launch'))
+  launch_dir = os.path.join(get_package_share_directory('launch_tutorial'), 'launch')
   turtlesim_world_1 = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource([launch_dir, 'turtlesim_world_1.launch.py'])
+    PythonLaunchDescriptionSource([launch_dir, '/turtlesim_world_1.launch.py'])
   )
   turtlesim_world_2 = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource([launch_dir, 'turtlesim_world_2.launch.py'])
+    PythonLaunchDescriptionSource([launch_dir, '/turtlesim_world_2.launch.py'])
   )
   turtlesim_world_2_with_namesapce = GroupAction(
     actions=[
@@ -23,17 +23,17 @@ def generate_launch_description():
   )
   
   broadcaster_listener_nodes = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource([launch_dir, 'turtlesim_world_2.launch.py']),
+    PythonLaunchDescriptionSource([launch_dir, '/turtlesim_world_2.launch.py']),
     launch_arguments={'target_frame': 'carrot1'}.items(),
   )
   mimic_nodes = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource([launch_dir, 'mimic.launch.py'])
+    PythonLaunchDescriptionSource([launch_dir, '/mimic.launch.py'])
   )
   fixed_frame_node = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource([launch_dir, 'fixed_broadcaster.launch.py'])
+    PythonLaunchDescriptionSource([launch_dir, '/fixed_broadcaster.launch.py'])
   )
   rviz_node = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource([launch_dir, 'turtlesim_rviz.launch.py'])
+    PythonLaunchDescriptionSource([launch_dir, '/turtlesim_rviz.launch.py'])
   )
   
   return LaunchDescription([
